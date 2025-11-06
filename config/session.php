02ea8f9172802d6e37ebddda2334b3e45001ce9b
MoRnINgStaR1994 <morningstarb19f6@gmail.com>
2025-11-06 08:40:10 -0800

    rm

--- config/session.php
+++ config/session.php
@@ -105,29 +105,11 @@ return [
 
     'domain' => env('SESSION_DOMAIN'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | HTTPS Only Cookies
-    |--------------------------------------------------------------------------
-    |
-    | By setting this option to true, session cookies will only be sent back
-    | to the server if the browser has a HTTPS connection. This will keep
-    | the cookie from being sent to you when it can't be done securely.
-    |
-    */
+
 
     'secure' => env('SESSION_SECURE_COOKIE'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | HTTP Access Only
-    |--------------------------------------------------------------------------
-    |
-    | Setting this value to true will prevent JavaScript from accessing the
-    | value of the cookie and the cookie will only be accessible through
-    | the HTTP protocol. It's unlikely you should disable this option.
-    |
-    */
+
 
     'http_only' => env('SESSION_HTTP_ONLY', true),
 
